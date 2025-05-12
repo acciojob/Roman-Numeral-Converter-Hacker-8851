983d3aeb764ecc0d15ed7f1939fd35db9422d9b5
Hacker-8851 <kumarsachin8851311@gmail.com>
2025-05-12 12:49:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -12,12 +12,11 @@ function convertToRoman(num) {
     
     let result = '';
     
-    // Loop through each Roman numeral symbol-value pair
     for (let i = 0; i < romanSymbols.length; i++) {
-        // While num is greater than or equal to the value of the Roman symbol
+       
         while (num >= romanSymbols[i][1]) {
-            result += romanSymbols[i][0]; // Append the Roman numeral symbol
-            num -= romanSymbols[i][1];    // Subtract the value from num
+            result += romanSymbols[i][0]; 
+            num -= romanSymbols[i][1];    
         }
     }
 
@@ -26,10 +25,6 @@ function convertToRoman(num) {
 
 // Test the function with various inputs
 console.log(convertToRoman(14));   // Output: XIV
-console.log(convertToRoman(798));  // Output: DCCXCVIII
-console.log(convertToRoman(1000)); // Output: M
-console.log(convertToRoman(4));    // Output: IV
-console.log(convertToRoman(36));   // Output: XXXVI
-console.log(convertToRoman(3999)); // Output: MMMCMXCIX
+
 
 module.exports = convertToRoman
