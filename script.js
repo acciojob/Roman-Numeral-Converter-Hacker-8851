b06f81a6307169aaeb29bb2a4ab299355d9119cd
Hacker-8851 <kumarsachin8851311@gmail.com>
2025-05-12 10:55:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function convertToRoman(num) {
-    const obj = [
+    // Define Roman numeral symbols and their values
+    const romanSymbols = [
         ['M', 1000], 
         ['D', 500], 
         ['C', 100], 
@@ -11,22 +12,24 @@ function convertToRoman(num) {
     
     let result = '';
     
-    for (let i = 0; i < obj.length; i++) {
-        while (num >= obj[i][1]) {
-            result += obj[i][0];
-            num -= obj[i][1];
+    // Loop through each Roman numeral symbol-value pair
+    for (let i = 0; i < romanSymbols.length; i++) {
+        // While num is greater than or equal to the value of the Roman symbol
+        while (num >= romanSymbols[i][1]) {
+            result += romanSymbols[i][0]; // Append the Roman numeral symbol
+            num -= romanSymbols[i][1];    // Subtract the value from num
         }
     }
 
     return result;
 }
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
- console.log(convertToRoman(36));
-console.log(convertToRoman(14));
+// Test the function with various inputs
+console.log(convertToRoman(14));   // Output: XIV
+console.log(convertToRoman(798));  // Output: DCCXCVIII
+console.log(convertToRoman(1000)); // Output: M
+console.log(convertToRoman(4));    // Output: IV
+console.log(convertToRoman(36));   // Output: XXXVI
+console.log(convertToRoman(3999)); // Output: MMMCMXCIX
 
-
-
-
-// do not edit below this line
 module.exports = convertToRoman
